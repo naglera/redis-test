caca9eee7f6e844253f36ff97303a1b19678fbd8
naglera <anagler123@gmail.com>
2023-01-23 09:27:45 +0000

    Revert "In the event of an assertion failure, hide command arguments from the operator"
    This reverts commit 1b4da82b0ccd9a44a31601ef39f463088eb379f1.

--- src/server.h
+++ src/server.h
@@ -1640,7 +1640,6 @@ struct redisServer {
 
     /* Configuration */
     int verbosity;                  /* Loglevel in redis.conf */
-    int hide_client_info;           /* In the event of an assertion failure, hide command arguments from the operator */
     int maxidletime;                /* Client timeout in seconds */
     int tcpkeepalive;               /* Set SO_KEEPALIVE if non-zero. */
     int active_expire_enabled;      /* Can be disabled for testing purposes. */
